592b59bd156455f2e46f8458fb7f29675c3b5b5b
madhur71 <73604803+madhur71@users.noreply.github.com>
2024-04-30 12:30:38 +0530

    material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { EducationRoutingModule } from './education/education-routing.module';
 import { EducationModule } from './education/education.module';
 import { HeaderComponent } from './header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @NgModule({
   declarations: [
@@ -13,9 +15,11 @@ import { HeaderComponent } from './header/header.component';
     HeaderComponent
   ],
   imports: [
+    MatSlideToggleModule,
     BrowserModule,
     AppRoutingModule,
-    EducationModule
+    EducationModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
