75ee02032a4e027adebc8a569a08eafe2b4585fb
madhur71 <73604803+madhur71@users.noreply.github.com>
2024-10-28 05:28:17 +0530

    Experience Fixed

--- src/app/education/about/about.component.ts
+++ src/app/education/about/about.component.ts
@@ -13,12 +13,14 @@ export class AboutComponent implements OnInit {
 
   ngOnInit(): void {
     var d1 = new Date("12/09/2021");   
-    var d2 = new Date("04/30/2024");   
+    var d2 = new Date();   
         
     var diff = d2.getTime() - d1.getTime();   
         
     this.experience = diff / (1000 * 60 * 60 * 24 * 365);  
     this.experience = this.experience.toFixed(2)
+
+    console.log(this.experience)
 
   }
 
